ffd109b81d0d699a5491e49b76d059877647179a
Jonathan Commins <joncom@gmail.com>
2013-07-07 18:04:07 -0700

    Created a form for uploading.

---
+++ form.php
@@ -0,0 +1,5 @@
+<form method="post" enctype="multipart/form-data">
+	<label for="file">Filename:</label>
+	<input type="file" name="file" id="file"><br>
+	<input type="submit" name="submit" value="Submit">
+</form>
