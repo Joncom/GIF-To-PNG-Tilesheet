6382dd3a6fbf8fc382620cf8b835ee56bf11ef9e
Jonathan Commins <joncom@gmail.com>
2013-07-07 18:08:22 -0700

    Added comments.

--- index.php
+++ index.php
@@ -1,14 +1,17 @@
 <?php
 
 if(!$_POST) {
+	// No file uploaded yet.
 	include "form.php";
 }
 else if ($_POST && $_FILES["file"]["error"] > 0)
 {
+	// File uploaded with problems.
 	echo "Error: " . $_FILES["file"]["error"] . "<br>";
 }
 else if($_POST && $_FILES["file"]["error"] === 0)
 {
+	// File uploaded successfully.
 	echo "Upload: " . $_FILES["file"]["name"] . "<br>";
 	echo "Type: " . $_FILES["file"]["type"] . "<br>";
 	echo "Size: " . ($_FILES["file"]["size"] / 1024) . " kB<br>";
