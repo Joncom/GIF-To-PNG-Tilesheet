2dc870cf6cfa87f13e3adf0b33857db9cb888b61
Jonathan Commins <joncom@gmail.com>
2013-07-07 17:55:48 -0700

    Fixed transparency.

--- index.php
+++ index.php
@@ -16,25 +16,22 @@ if (GifFrameExtractor::isAnimatedGif($gifFilePath)) { // check this is an animat
 
     $frameCount = $gfe->getFrameNumber();
 
-    $tilesheet = imagecreate( $width * $frameCount, $height );
+    $tilesheet = imagecreate($width * $frameCount, $height);
+
+    $red = imagecolorallocate($tilesheet, 255, 0, 0);
+    imagecolortransparent($tilesheet, $red);
 
     $x = 0;
     $y = 0;
 
     foreach ($gfe->getFrames() as $frame) {
-
         $frame = $frame['image'];
-
         imagecopy($tilesheet, $frame, $x, $y, 0, 0, $width, $height);
-
         $x += $width;
-
     }
 
     header( "Content-type: image/png" );
     imagepng($tilesheet);
-
-
 
 }
 
